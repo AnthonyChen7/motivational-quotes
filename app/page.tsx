abe8c74463d9bca297eb26802c4a874a1a3cf23f
DESKTOP-I9T7QHB\hp_a1 <achen7x7@gmail.com>
2026-02-13 12:53:25 -0800

    added sign out button

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
 
       const {error} = await supabase.auth.signInWithOAuth({
         provider: 'google',
-
       });
 
       if (error) {
@@ -40,6 +39,7 @@ export default function Home() {
   return (
     <>
     <button onClick={() => loginWithGoogle()}>Log in with Google</button>
+    <button onClick={async() => { await supabase.auth.signOut(); }}>Sign Out</button>
       <Chat />
     </>
   );
