897ba0c6b8fa338dd20be77b041d26a3a1bf130b
DESKTOP-I9T7QHB\hp_a1 <achen7x7@gmail.com>
2026-02-19 15:32:08 -0800

    undid files

--- app/lib/supabase/actions.ts
+++ app/lib/supabase/actions.ts
@@ -5,29 +5,14 @@ import createClient from "./server";
 export async function createQuote({userId, quote}: {userId: string, quote: string}) {
     const supabase = await createClient();
     const {data, error} = await supabase.from('quote')
-    .upsert([
+    .insert([
         {
             user_id: userId,
             quote
         }
-    ], {
-        onConflict: 'user_id, quote',
-        ignoreDuplicates: true
-    }).select();
+    ]);
     if (error) {
         console.error('error inserting quote', error);
-        return {success: false, error: error.message}
-    }
-    return {success: true, data}
-}
-
-export async function getQuote({userId, quote}: {userId: string, quote: string}) {
-    const supabase = await createClient();
-    const {data, error} = await supabase.from('quote')
-    .select('user_id, quote')
-    .eq(userId, quote);
-    if (error) {
-        console.error('error finding quote', error);
         return {success: false, error: error.message}
     }
     return {success: true, data}
